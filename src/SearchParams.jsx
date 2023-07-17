d200da73b14a43c71bc32bd99017aaf7459968e1
lokicodedaily <you@example.com>
2023-07-18 01:54:56 +0530

    create animal and breed option

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
 
   return (
     <div className="search-params">
@@ -14,6 +18,40 @@ const SearchParams = () => {
             id="location"
             placeholder="Location"
           />
+        </label>
+
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        <label htmlFor="animal">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+          >
+            <option />
+            {breeds.map((breed) => {
+              <option key={breed}>{breed}</option>;
+            })}
+          </select>
         </label>
         <button>Submit</button>
       </form>
